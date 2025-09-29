908ff4b205bd8784744ffcecd77bd635749da86c
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-29 22:31:22 +0500

    working on the result display

---
+++ frontend/src/components/ResultDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export default function ResultDisplay({ swot }) {
+}
+
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
