8dc2513a085da8d3fa614208283aef8cd15929d4
Eltaf-azizi <iltfhuss@gmail.com>
2025-09-29 22:37:20 +0500

    working on the result display function

--- frontend/src/components/ResultDisplay.js
+++ frontend/src/components/ResultDisplay.js
@@ -3,6 +3,26 @@ import React from 'react';
 
 
 export default function ResultDisplay({ swot }) {
+  return (
+
+    <div className="result">
+      {["strengths", "weaknesses", "opportunities", "threats"].map((key) => (
+        <div key={key} className={`swot-section ${key}`}>
+          <h2>{capitalizeFirstLetter(key)}</h2>
+          {swot[key].length > 0 ? (
+            <ul>
+              {swot[key].map((item, idx) => (
+                <li key={idx}>{item}</li>
+              ))}
+            </ul>
+          ) : (
+            <p>No insights found.</p>
+          )}
+        </div>
+      ))}
+    </div>
+    
+  );
 }
 
 
