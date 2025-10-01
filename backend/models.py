edfeef9bc25d3f06af55ace3b338db7587265c7a
Eltaf-azizi <iltfhuss@gmail.com>
2025-10-01 22:08:35 +0500

    working on the model user

---
+++ backend/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+Base = declarative_base()
+
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
